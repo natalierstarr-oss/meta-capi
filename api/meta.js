4a42c33b5a83a364860feee7c1adebfa6be4b9ef
natalierstarr-oss <natalie.r.starr@gmail.com>
2026-04-22 12:07:47 -0400

    Update meta.js

--- api/meta.js
+++ api/meta.js
@@ -1,8 +1,8 @@
 export default async function handler(req, res) {
-  const pixel = "594259826536475";
-  const token = "EAAcXbP1YQ78BRdhyIycn0MSAN4Tcjx0ZA7prmWsjounsT4ES1fWW4Fd7f9mer51R2V1RwZAxrpdvyIEschRYecYSxy9uOrYH1UzWS50BN79lvbl3qugojwMJVvZCj3Tqzxhz2pcP4Nabm5A0XhhxhwCfqiCKEz8PtfrbiPWJMMnWSEJzg4ZCD1kfzFvIZBQZDZD";
-
   try {
+    const pixel = "594259826536475";
+    const token = "EAAcXbP1YQ78BRdhyIycn0MSAN4Tcjx0ZA7prmWsjounsT4ES1fWW4Fd7f9mer51R2V1RwZAxrpdvyIEschRYecYSxy9uOrYH1UzWS50BN79lvbl3qugojwMJVvZCj3Tqzxhz2pcP4Nabm5A0XhhxhwCfqiCKEz8PtfrbiPWJMMnWSEJzg4ZCD1kfzFvIZBQZDZD";
+
     const response = await fetch(
       `https://graph.facebook.com/v18.0/${pixel}/events?access_token=${token}`,
       {
@@ -22,6 +22,7 @@ export default async function handler(req, res) {
       }
     );
 
+    // IMPORTANT: do NOT parse JSON (this is where crashes happen)
     const text = await response.text();
 
     return res.status(200).send(text);
