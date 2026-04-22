07b0a301cc24ab9ed55bc2e55447bb302a233234
natalierstarr-oss <natalie.r.starr@gmail.com>
2026-04-22 12:06:36 -0400

    Update meta.js

--- api/meta.js
+++ api/meta.js
@@ -1,8 +1,8 @@
 export default async function handler(req, res) {
-  try {
-    const pixel = "594259826536475";
-    const token = "EAAcXbP1YQ78BRdhyIycn0MSAN4Tcjx0ZA7prmWsjounsT4ES1fWW4Fd7f9mer51R2V1RwZAxrpdvyIEschRYecYSxy9uOrYH1UzWS50BN79lvbl3qugojwMJVvZCj3Tqzxhz2pcP4Nabm5A0XhhxhwCfqiCKEz8PtfrbiPWJMMnWSEJzg4ZCD1kfzFvIZBQZDZD";
+  const pixel = "594259826536475";
+  const token = "EAAcXbP1YQ78BRdhyIycn0MSAN4Tcjx0ZA7prmWsjounsT4ES1fWW4Fd7f9mer51R2V1RwZAxrpdvyIEschRYecYSxy9uOrYH1UzWS50BN79lvbl3qugojwMJVvZCj3Tqzxhz2pcP4Nabm5A0XhhxhwCfqiCKEz8PtfrbiPWJMMnWSEJzg4ZCD1kfzFvIZBQZDZD";
 
+  try {
     const response = await fetch(
       `https://graph.facebook.com/v18.0/${pixel}/events?access_token=${token}`,
       {
@@ -22,12 +22,11 @@ export default async function handler(req, res) {
       }
     );
 
-    const data = await response.json();
+    const text = await response.text();
 
-    return res.status(200).json(data);
+    return res.status(200).send(text);
 
-  } catch (err) {
-    console.error("ERROR:", err);
-    return res.status(500).json({ error: err.message });
+  } catch (error) {
+    return res.status(500).send("ERROR: " + error.message);
   }
 }
