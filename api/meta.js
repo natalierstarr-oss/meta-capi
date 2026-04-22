8ed108ec15674bcf512eba4d6277f76e0f1f29cf
natalierstarr-oss <natalie.r.starr@gmail.com>
2026-04-22 12:20:58 -0400

    Update meta.js

--- api/meta.js
+++ api/meta.js
@@ -1,31 +1,3 @@
-module.exports = async function handler(req, res) {
-  // respond immediately so webhook succeeds
+export default function handler(req, res) {
   res.status(200).send("ok");
-
-  try {
-    const pixel = "594259826536475";
-    const token = "EAAcXbP1YQ78BRdhyIycn0MSAN4Tcjx0ZA7prmWsjounsT4ES1fWW4Fd7f9mer51R2V1RwZAxrpdvyIEschRYecYSxy9uOrYH1UzWS50BN79lvbl3qugojwMJVvZCj3Tqzxhz2pcP4Nabm5A0XhhxhwCfqiCKEz8PtfrbiPWJMMnWSEJzg4ZCD1kfzFvIZBQZDZD";
-
-    await fetch(
-      `https://graph.facebook.com/v18.0/${pixel}/events?access_token=${token}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          data: [
-            {
-              event_name: "Purchase",
-              event_time: Math.floor(Date.now() / 1000),
-              action_source: "website"
-            }
-          ]
-        })
-      }
-    );
-
-  } catch (err) {
-    console.error("META ERROR:", err);
-  }
-};
+}
