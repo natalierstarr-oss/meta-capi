53bc0ba0b309d306e173390ce9c3bca619399789
natalierstarr-oss <natalie.r.starr@gmail.com>
2026-04-22 11:09:57 -0400

    Update meta.js

--- api/meta.js
+++ api/meta.js
@@ -2,33 +2,22 @@ export default async function handler(req, res) {
   try {
     const pixel = "594259826536475";
     const token = "EAAMqbiedbBsBRR6oNsxTPZCMgR4pZCsu3fL7tjD1zoZCtATkZCTuhvZB14H7HJoxFVZBz8N1sQ94b1mwqIHdZCDB5QzW4m47Fcb4OHRbOZCsVQG9VwULfpr8PVSkBBNdaWr901Ylc4ZCU86ysWz44MGYqZC7GIe5wqqyT4Jw9pZCsBB9wUL2PHUcZAABBBYJlWbpHwZDZD";
-
-    const response = await fetch(
-      `https://graph.facebook.com/v18.0/${pixel}/events?access_token=${token}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          data: [
-            {
-              event_name: "Purchase",
-              event_time: Math.floor(Date.now() / 1000),
-              action_source: "website"
-            }
-          ]
-        })
-      }
-    );
-
-    const data = await response.json();
-    console.log("META RESPONSE:", data);
-
-    res.status(200).json(data);
-
-  } catch (err) {
-    console.error(err);
-    res.status(200).json({ error: "fail" });
+const response = await fetch(
+  `https://graph.facebook.com/v18.0/${pixel}/events`,
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`
+    },
+    body: JSON.stringify({
+      data: [
+        {
+          event_name: "Purchase",
+          event_time: Math.floor(Date.now() / 1000),
+          action_source: "website"
+        }
+      ]
+    })
   }
-}
+);
