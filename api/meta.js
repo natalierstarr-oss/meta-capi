f40b3847a7757fcee8a8e367e3ac52d7d5eac739
natalierstarr-oss <natalie.r.starr@gmail.com>
2026-04-22 11:51:28 -0400

    Update meta.js

--- api/meta.js
+++ api/meta.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   try {
     const pixel = "594259826536475";
-    const token = "EAAMqbiedbBsBRR6oNsxTPZCMgR4pZCsu3fL7tjD1zoZCtATkZCTuhvZB14H7HJoxFVZBz8N1sQ94b1mwqIHdZCDB5QzW4m47Fcb4OHRbOZCsVQG9VwULfpr8PVSkBBNdaWr901Ylc4ZCU86ysWz44MGYqZC7GIe5wqqyT4Jw9pZCsBB9wUL2PHUcZAABBBYJlWbpHwZDZD";
+    const token = "EAAcXbP1YQ78BRU5E8xSkSrE3ZC8G6T2IbkXoC0Bn6ZCXyVDykZCwBZAloewYyBiUAVeR0SYuuTLtfCjFtLwmG7Wq68Tbxpm3HHglw7ZCJLF7TuIJ7t4tkVwQ0th8rNdVBbZBHhZBFesgUTxAnZCZAGTZCfUYo4r5zGujmjWrd9z6v4cINBDVWIG0uM2i3i07whOwZDZD";
 const response = await fetch(
   `https://graph.facebook.com/v18.0/${pixel}/events`,
   {
